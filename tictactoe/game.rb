9a6eaa4b8e0d2bf59461ab334f9c84a71eaa4ef5
Tenacious-Qi <joe.mccann@mailbox.org>
2020-04-30 00:31:48 -0400

    moved 'check who wins' methods into game.rb

--- tictactoe/game.rb
+++ tictactoe/game.rb
@@ -62,4 +62,38 @@ class Game
       prompt_to_play_again
     end
   end
+
+  def check_if_player_one_wins
+    win = ["X", "X", "X"] #@board.cells.values_at(0,1,2)
+    case
+    #rows
+    when @board.cells.values_at(0,1,2) == win then @player1.winner = true
+    when @board.cells.values_at(3,4,5) == win then @player1.winner = true
+    when @board.cells.values_at(6,7,8) == win then @player1.winner = true
+    #columns
+    when @board.cells.values_at(0,3,6) == win then @player1.winner = true
+    when @board.cells.values_at(1,4,7) == win then @player1.winner = true
+    when @board.cells.values_at(2,5,8) == win then @player1.winner = true
+    #diagonals
+    when @board.cells.values_at(0,4,8) == win then @player1.winner = true
+    when @board.cells.values_at(2,4,6) == win then @player1.winner = true
+    end
+  end
+
+  def check_if_player_two_wins
+    win = ["O", "O", "O"]
+    case
+    #rows
+    when @board.cells.values_at(0,1,2) == win then @player2.winner = true
+    when @board.cells.values_at(3,4,5) == win then @player2.winner = true
+    when @board.cells.values_at(6,7,8) == win then @player2.winner = true
+    #columns
+    when @board.cells.values_at(0,3,6) == win then @player2.winner = true
+    when @board.cells.values_at(1,4,7) == win then @player2.winner = true
+    when @board.cells.values_at(2,5,8) == win then @player2.winner = true
+    #diagonals
+    when @board.cells.values_at(0,4,8) == win then @player2.winner = true
+    when @board.cells.values_at(2,4,6) == win then @player2.winner = true
+    end
+  end
 end
