a423548d163bde73b7794ae074168e342b7887a4
Tenacious-Qi <joe.mccann@mailbox.org>
2020-05-04 17:56:50 -0400

    added some methods to Game

--- mastermind/main.rb
+++ mastermind/main.rb
@@ -10,13 +10,13 @@ class Board
     @number_of_rounds = 0
   end
   
-  def display(round_number)
+  def display
 
     puts <<-HEREDOC
 
-                                          #{indicators[1]} #{indicators[2]}       
-       #{round_number}: #{positions[1]}  #{positions[2]}  #{positions[3]}  #{positions[4]}
-                                          #{indicators[3]} #{indicators[4]}
+                                          #{indicators[0]} #{indicators[1]}       
+        #{positions[0]}  #{positions[1]}  #{positions[2]}  #{positions[3]}
+                                          #{indicators[2]} #{indicators[3]}
 
     HEREDOC
   end
@@ -51,9 +51,12 @@ class CodeMaker
     random_nums = []
     #prevent duplicates. only push to winning_code if color not present. ensure there are 4 colors randomly selected
     until random_nums.count == 4
-      random_num = (rand * 6).floor + 1
+      random_num = (rand * 5).floor
       print "#{random_num} "
         case
+        when random_num == 0 && !winning_code.include?(colors[0])
+          random_nums << random_num
+          winning_code << colors[0]
         when random_num == 1 && !winning_code.include?(colors[1])
           random_nums << random_num
           winning_code << colors[1]
@@ -64,25 +67,15 @@ class CodeMaker
           random_nums << random_num
           winning_code << colors[3]
         when random_num == 4 && !winning_code.include?(colors[4])
-          random_nums << random_num
           winning_code << colors[4]
+          random_nums << random_num
         when random_num == 5 && !winning_code.include?(colors[5])
           winning_code << colors[5]
-          random_nums << random_num
-        when random_num == 6 && !winning_code.include?(colors[6])
-          winning_code << colors[6]
           random_nums << random_num
         end
     end
     @winning_code = winning_code
     winning_code
-  end
-  
-  def provide_feedback
-    #logic for determining how to color indicators
-    if @board.positions[1] == @winning_code[0]
-      puts "first matches"
-    end
   end
 end
 
@@ -98,40 +91,73 @@ class Game
     @codemaker.generate_colors
   end
 
+  def show_message
+    puts <<-HEREDOC
+
+    Welcome to Mastermind!
+
+    We will play 10 rounds. Try to guess code in 10 rounds.
+    Code will be revealed after 10 rounds or upon guessing correct code.
+
+                            #{"red".red} #{"green".green} #{"purple".magenta}
+    Your color choices are: 
+                            #{"yellow".yellow} #{"blue".blue} #{"black".light_black}
+
+    HEREDOC
+  end
+
+  def show_color_choices
+    puts <<-HEREDOC
+
+                            #{"red".red} #{"green".green} #{"purple".magenta}
+    Your color choices are: 
+                            #{"yellow".yellow} #{"blue".blue} #{"black".light_black}
+
+    HEREDOC
+  end
+
   def guess
-    round_complete = false
     #after four colors are placed, increment @number_of_rounds by 1. 
     #display board with positions updated to show colors
-    i = 1
-    until round_complete
-      puts "make a guess at position #{i}"
-      color = gets.chomp.strip.downcase
-      @board.positions[i] = color
-      @board.colors_placed += 1
-      i += 1
-      round_complete = true if @board.colors_placed == 4
+    show_message
+    until @board.number_of_rounds == 10
+      round_complete = false
+      i = 0
+      until round_complete
+        print "make a guess at position #{i + 1}: "
+        color = gets.chomp.strip.downcase
+        @board.positions[i] = color
+        @board.colors_placed += 1
+        i += 1
+        round_complete = true if @board.colors_placed == 4
+      end
+      if round_complete
+        @board.increment_number_of_rounds
+        provide_feedback
+        @board.display
+      end
+      reset_positions
     end
-    if round_complete
-      provide_feedback
-      @board.increment_number_of_rounds
-      @board.display(@board.number_of_rounds)
-    end
-    @board.colors_placed = 0
-    @board.positions = {1=>"[ ]", 2=>"[ ]", 3=>"[ ]", 4=>"[ ]"}   
+    @board.number_of_rounds = 0  
   end
 
   def winning_code
     @codemaker.winning_code
   end
 
+  def reset_positions
+    @board.colors_placed = 0
+    @board.positions = {1=>"[ ]", 2=>"[ ]", 3=>"[ ]", 4=>"[ ]"} 
+  end
+
   def provide_feedback
-    puts "here is some feedback"
-    if @board.positions[1] == winning_code[0]
-      puts "one color has matched"
+    puts "\nresults of round #{@board.number_of_rounds}: \n"
+    if @board.positions[0] == winning_code[0]
+      puts "\none color has matched"
     end
   end
 end
 
-@board = Board.new({1=>"[ ]", 2=>"[ ]", 3=>"[ ]", 4=>"[ ]"}, {1=>"@", 2=>"@", 3=> "@", 4=>"@"})
-@codemaker = CodeMaker.new(["placeholder", "red", "green", "purple", "yellow", "orange", "brown"])
+@board = Board.new({0=>"[ ]", 1=>"[ ]", 2=>"[ ]", 3=>"[ ]"}, {0=>"@", 1=>"@", 2=> "@", 3=>"@"})
+@codemaker = CodeMaker.new(["red", "green", "purple", "yellow", "blue", "black"])
 @game = Game.new(@codemaker, @board)
